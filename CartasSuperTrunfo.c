b9e2bb8e33152348c6a329e96664387e3a206dab
gutembergmnmb1993 <153691575+gutembergmnmb1993@users.noreply.github.com>
2025-09-14 15:06:49 -0300

    Update CartasSuperTrunfo.c
    conclusão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,81 +1,105 @@
 #include <stdio.h>
 
 int main() {
-    char estado[2];
-    char codigo_carta[20];
-    char nome_cidade[20];
-    int populacao;
-    float area;
-    float pib;
-    int numero_pontos_turisticos;
-    
-    
-    //Carta1
+    // Carta 1
+    char estado1[3];
+    char codigo_carta1[20];
+    char nome_cidade1[20];
+    unsigned long int populacao1;
+    float area1;
+    float pib1;
+    int numero_pontos_turisticos1;
+    float densidade_populacional1;
+    float per_capita1;
+    float super_poder1;
 
+    // Carta 2
+    char estado2[3];
+    char codigo_carta2[20];
+    char nome_cidade2[20];
+    unsigned long int populacao2;
+    float area2;
+    float pib2;
+    int numero_pontos_turisticos2;
+    float densidade_populacional2;
+    float per_capita2;
+    float super_poder2;
+
+    // ===== CARTA 1 =====
     printf("Digite o estado (duas letras): ");
-    scanf(" %s", &estado); //salvo na variavel estado
-    printf("O estado digitado foi: %s\n", estado);
+    scanf("%s", estado1);
     
-    printf("digite o codigo da carta:");
-    scanf(" %s", codigo_carta);
-    printf("o codigo da carta é: %s\n", codigo_carta);
+    printf("Digite o codigo da carta: ");
+    scanf("%s", codigo_carta1);
     
-    printf("digite o nome da cidade:");
-    scanf(" %s", nome_cidade);
-    printf("o nome da cidade é: %s\n", nome_cidade);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome_cidade1);
     
-    printf("digite a população de %s:", nome_cidade);
-    scanf(" %d", &populacao);
-    printf("a população da cidade de %s é :%d\n",nome_cidade, populacao);
+    printf("Digite a população de %s: ", nome_cidade1);
+    scanf("%lu", &populacao1);
     
-    printf("a aréa de %s, é?: ", nome_cidade);
-    scanf("%f", &area);
-    printf(" a area digitada da cidade de %s, foi: %f\n", nome_cidade, area);
+    printf("Digite a área de %s: ", nome_cidade1);
+    scanf("%f", &area1);
     
-    printf("o pib de %s, é?:", nome_cidade);
-    scanf(" %f", &pib);
-    printf("o pib da cidade de %s é: %f\n", nome_cidade, pib);
+    printf("Digite o PIB de %s: ", nome_cidade1);
+    scanf("%f", &pib1);
     
-    printf("o numero de pontos turisticos de %s é?:", nome_cidade);
-    scanf(" %d,", &numero_pontos_turisticos);
-    printf(" o numero de pontos turisticos de %s é :%d\n", nome_cidade, numero_pontos_turisticos);
+    printf("Digite o número de pontos turísticos de %s: ", nome_cidade1);
+    scanf("%d", &numero_pontos_turisticos1);
+   
+    densidade_populacional1 = (float) populacao1 / area1;
+    per_capita1 = (float) pib1 / populacao1;
+    super_poder1 = (float)(populacao1 + area1 + pib1 + per_capita1 + densidade_populacional1);
     
+    // ===== CARTA 2 =====
+    printf("\n===== Agora digite os dados da Carta 2 =====\n\n");
     
-    printf("agora você irá digitar os dados da carta 2\n");
-    
-    
-    
-    
-    //Carta2
     printf("Digite o estado (duas letras): ");
-    scanf(" %s", &estado); //salvo na variavel estado
-    printf("O estado digitado foi: %s\n", estado);
+    scanf("%s", estado2);
     
-    printf("digite o codigo da carta:");
-    scanf(" %s", codigo_carta);
-    printf("o codigo da carta é: %s\n", codigo_carta);
+    printf("Digite o codigo da carta: ");
+    scanf("%s", codigo_carta2);
     
-    printf("digite o nome da cidade:");
-    scanf(" %s", nome_cidade);
-    printf("o nome da cidade é: %s\n", nome_cidade);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome_cidade2);
     
-    printf("digite a população de %s:", nome_cidade);
-    scanf(" %d", &populacao);
-    printf("a população da cidade de %s é :%d\n",nome_cidade, populacao);
+    printf("Digite a população de %s: ", nome_cidade2);
+    scanf("%lu", &populacao2);
     
-    printf("a aréa de %s, é?: ", nome_cidade);
-    scanf("%f", &area);
-    printf(" a area digitada da cidade de %s, foi: %f\n", nome_cidade, area);
+    printf("Digite a área de %s: ", nome_cidade2);
+    scanf("%f", &area2);
     
-    printf("o pib de %s, é?:", nome_cidade);
-    scanf(" %f", &pib);
-    printf("o pib da cidade de %s é: %f\n", nome_cidade, pib);
+    printf("Digite o PIB de %s: ", nome_cidade2);
+    scanf("%f", &pib2);
     
-    printf("o numero de pontos turisticos de %s é?:", nome_cidade);
-    scanf(" %d,", &numero_pontos_turisticos);
-    printf(" o numero de pontos turisticos de %s é :%d", nome_cidade, numero_pontos_turisticos);
-    
-    
-    
+    printf("Digite o número de pontos turísticos de %s: ", nome_cidade2);
+    scanf("%d", &numero_pontos_turisticos2);
+   
+    densidade_populacional2 = (float) populacao2 / area2;
+    per_capita2 = (float) pib2 / populacao2;
+    super_poder2 = (float)(populacao2 + area2 + pib2 + per_capita2 + densidade_populacional2);
+
+    // ===== Comparação =====
+    printf("\n===== COMPARAÇÃO =====\n");
+    printf("Super poder Carta 1: %.2f\n", super_poder1);
+    printf("Super poder Carta 2: %.2f\n", super_poder2);
+
+    // ===== Comparação das cartas usando apenas booleanos =====
+    int resultado_populacao = populacao1 > populacao2;
+    int resultado_area = area1 > area2;
+    int resultado_pib = pib1 > pib2;
+    int resultado_pontos_turisticos = numero_pontos_turisticos1 > numero_pontos_turisticos2;
+    int resultado_den_populacional = densidade_populacional1 > densidade_populacional2;
+    int resultado_pib_percapita = per_capita1 > per_capita2;
+    int resultado_super_poder = super_poder1 > super_poder2;
+
+    printf("População: Carta 1 venceu? %d\n", resultado_populacao);
+    printf("Área: Carta 1 venceu? %d\n", resultado_area);
+    printf("PIB: Carta 1 venceu? %d\n", resultado_pib);
+    printf("Pontos turísticos: Carta 1 venceu? %d\n", resultado_pontos_turisticos);
+    printf("Densidade populacional: Carta 1 venceu? %d\n", resultado_den_populacional);
+    printf("PIB per capita: Carta 1 venceu? %d\n", resultado_pib_percapita);
+    printf("Super Poder: Carta 1 venceu? %d\n", resultado_super_poder);
+
     return 0;
 }
